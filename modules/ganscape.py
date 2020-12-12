7f4d5dcbace011ceda9e3cba46cae1bf007dc745
Dulacska Matyas <dulacskamty@gmail.com>
2020-12-12 15:49:22 +0100

    Update network class for handling load model both for prediction and for training

--- modules/ganscape.py
+++ modules/ganscape.py
@@ -9,6 +9,8 @@ from keras.callbacks import TensorBoard
 import keras.backend as K
 import tensorflow as tf
 
+import os
+
 from network_structure import buildGenerator, buildDiscriminator
 from custom_losses import generatorMSELossInJoint, generatorAdversarialLoss, discriminatorAdversarialLoss
 
@@ -20,11 +22,12 @@ class GANScape():
         self.batchSize = batchSize
         self.inputImgShape = inputImgShape
         self.localDiscInputShape = localDiscInputShape
-        self.weightForAdversarialInJointLoss = 0.1
         
+        self.generatorDescriber = generatorDescriber
+        self.discriminatorDescriber = discriminatorDescriber
         self.optimizers = optimizers
         self.weightsForJointLoss = weightsForJointLoss
-        
+
 
         """
         #   One block = [Conv2D] + [ReLU] + [BatchNorm]
@@ -35,10 +38,8 @@ class GANScape():
               BatchNormalization()
         """
 
-        self.generatorDescriber = generatorDescriber
-        self.discriminatorDescriber = discriminatorDescriber
 
-        
+
     def compileModel(self, model, losses, optimizer, lossWeights=None):
         if lossWeights is None:
             model.compile(loss = losses,optimizer = optimizer)
@@ -66,7 +67,7 @@ class GANScape():
             realImgLocalInput = tf.image.crop_to_bounding_box(
                 real, top, left, h, w)
             realLocalInputs.append(realImgLocalInput)
-            
+
             fakeImgLocalInput = tf.image.crop_to_bounding_box(
                 fake, top, left, h, w)
             fakeLocalInputs.append(fakeImgLocalInput)
@@ -76,37 +77,48 @@ class GANScape():
         fakeLocalInputs = K.stack(fakeLocalInputs)
 
         return [realLocalInputs,  fakeLocalInputs]
+
+
+
+    def buildAndCompile(self, loadModels=False, loadPath=None):
+        #generator input
+        inputImgGen = None
         
-    
-    
-    def buildAndCompile(self, generatorDescriber=None, discriminatorDescriber=None, optimizers=None, weightsForJointLoss=None, pathForLoadModel=None):
-        #define inputs
-        genInputShape = (self.inputImgShape[0], self.inputImgShape[1], self.inputImgShape[2])
-        inputImgGen = Input(genInputShape, name='input_image_layer_for_generator')
-        
-        maskParamsInputLocalDisc = Input(shape=4, name='input_mask_params_for_local_disc')
-        realInputLocalDisc = Input(shape=self.localDiscInputShape, name='input_real_mask_img_for_local_disc')
-        
-        realInputGlobalDisc = Input(shape=self.inputImgShape, name='input_real_img_for_global_disc')
-
-        #handling loading model if needed
-        if pathForLoadModel != None:
-            self.generator = buildGenerator(inputImgGen, generatorDescriber)
-            self.discriminator = buildDiscriminator(realInputGlobalDisc, realInputLocalDisc, discriminatorDescriber)
-
-        else:
-            self.generator = buildGenerator(inputImgGen, generatorDescriber)
-            self.discriminator = buildDiscriminator(realInputGlobalDisc, realInputLocalDisc, discriminatorDescriber)
-
-
+        #discriminator inputs
+        realInputGlobalDisc = None
+        realInputLocalDisc = None
             
+        #mask layer between generator and local discriminator
+        maskParamsInputLocalDisc = Input(shape=4, name='input_mask_params_for_local_disc')
+        
+        
+        if not loadModels:
+            #generator: define input and build
+            inputImgGen = Input(self.inputImgShape, name='input_image_layer_for_generator')
+            self.generator = buildGenerator(inputImgGen, self.generatorDescriber)
+            #discriminator: define inputs and build
+            realInputGlobalDisc = Input(shape=self.inputImgShape, name='input_real_img_for_global_disc')
+            realInputLocalDisc = Input(shape=self.localDiscInputShape, name='input_real_mask_img_for_local_disc')
+            self.discriminator = buildDiscriminator(realInputGlobalDisc, realInputLocalDisc, self.discriminatorDescriber)
+        else:
+            generator, discriminator = self.loadAndGetModels(loadPath)
+            #generator
+            inputImgGen = generator.input
+            self.generator = generator
+            #discriminator
+            realInputGlobalDisc = discriminator.inputs[0]
+            realInputLocalDisc = discriminator.inputs[1]
+            self.discriminator = discriminator
+
+
+
         fakeInputGlobalDisc = self.generator.layers[-1].output
-    
+
         # local discriminator get the output of generator through a Lamda layer that cuts the inpainted part of the image
         realInputLocalDisc, fakeInputLocalDisc = Lambda(lambda x: self.cropLocalDiscInputFromImg(*x),name='crop_local_disc_input_from_img')([realInputGlobalDisc, fakeInputGlobalDisc, maskParamsInputLocalDisc])
         realProb =  self.discriminator([realInputGlobalDisc, realInputLocalDisc])
         fakeProb =  self.discriminator([fakeInputGlobalDisc, fakeInputLocalDisc])
-        
+
         #print("prob_real: ", realProb)
         #print("prob_fake: ", fakeProb)
         def _stack(pReal, pFake):
@@ -115,27 +127,30 @@ class GANScape():
             return prob
         probs = Lambda(lambda x: _stack(*x), name='stack_prob')([realProb, fakeProb])
         # print("prob: ", prob)
-        
+
         outputsForOnlyDisc = [probs]
         lossesForOnlyDisc = [discriminatorAdversarialLoss]
         outputsForOnlyGenJoint = [fakeInputGlobalDisc, probs]
         lossesForOnlyGenJoint = [generatorMSELossInJoint, generatorAdversarialLoss]
-        lossWeightForOnlyGenJoint = [weightsForJointLoss["mse"], weightsForJointLoss["adversarial"]]
-        
+        lossWeightForOnlyGenJoint = [self.weightsForJointLoss["mse"], self.weightsForJointLoss["adversarial"]]
+
         ########################################################x
         #COMPILE TRAINING MODELS - we will have 3 different compiled variables (but the same network) becasuse we need:
         #   -different losses
         #   -different input-output combinations
         # in the 3 different phases of training
-        
+
         combinedModelTrainOnlyDisc = Model([inputImgGen, maskParamsInputLocalDisc, realInputGlobalDisc], outputsForOnlyDisc)
         combinedModelTrainOnlyGenWithJoint = Model([inputImgGen, maskParamsInputLocalDisc, realInputGlobalDisc], outputsForOnlyGenJoint)
-        
-        self.modelForTrainOnlyGenWithMSE = self.compileModel(self.generator, 'mean_squared_error', optimizers["onlyGenMse"])
-        self.modelForTrainOnlyDisc = self.compileModel(combinedModelTrainOnlyDisc, lossesForOnlyDisc, optimizers["onlyDisc"])
-        self.modelForTrainOnlyGenWithJoint = self.compileModel(combinedModelTrainOnlyGenWithJoint, lossesForOnlyGenJoint, optimizers["onlyGenJoint"], lossWeights=lossWeightForOnlyGenJoint)
-        
+
+        self.modelForTrainOnlyGenWithMSE = self.compileModel(self.generator, 'mean_squared_error', self.optimizers["onlyGenMse"])
+        self.modelForTrainOnlyDisc = self.compileModel(combinedModelTrainOnlyDisc, lossesForOnlyDisc, self.optimizers["onlyDisc"])
+        self.modelForTrainOnlyGenWithJoint = self.compileModel(combinedModelTrainOnlyGenWithJoint, lossesForOnlyGenJoint, self.optimizers["onlyGenJoint"], lossWeights=lossWeightForOnlyGenJoint)
+
         #activatedGenOutput = Activation(activations.sigmoid)(self.generator.output)
         self.generatorForPredict = Model(inputImgGen, self.generator.output)
 
-    
+    def loadAndGetModels(self, pathForLoadModel):
+        generator = load_model(os.path.join(pathForLoadModel, 'generator'))
+        discriminator = load_model(os.path.join(pathForLoadModel, 'discriminator'))
+        return generator, discriminator
